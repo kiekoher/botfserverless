9ca98a18da4fda0fd9a0409f922f4ae0bed1ec00
Diego Her <admin@rdv.net.co>
2025-07-24 00:10:02 -0500

    fix puppeteer launch in docker

--- bot.js
+++ bot.js
@@ -20,9 +20,15 @@ const client = new Client({
     puppeteer: {
         headless: true,
         args: [
-            '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote',
-            '--single-process', '--disable-gpu'
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu',
+            '--disable-crash-reporter',
+            '--disable-crashpad'
         ]
     }
 });
