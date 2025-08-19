e2f5e65a03747cace2a14577015c96aab2fb2f12
Diego Her <admin@rdv.net.co>
2025-08-19 15:20:35 -0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   typescript: { ignoreBuildErrors: false },
-  // eslint: { ignoreDuringBuilds: true },
 }
 export default nextConfig
